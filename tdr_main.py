198d470833eec7f37e6900c810799a8a1481a47b
triter3 <playtriter3@gmail.com>
2015-11-18 16:55:22 +0100

    act

--- tdr_main.py
+++ tdr_main.py
@@ -1,39 +1,57 @@
-import time
-import bge
-import tdr_reader
-import math
+import time #libreria de python
+import bge #llibreria de Blender
+import tdr_reader #s'imporen les funcións del fitxer reader.py
+import math #llibreria de python
 
 
-afterTime = 0
-own = 0
+afterTime = 0 #varable global
+own = 0 #variable global
+FPS = 20 #variable global que conté el valor de fotorgames per segon que te d'anar el programa
 
-def running(cont):
-    global own
+def runningUp(cont): #funció que es crida desde un modul del del "Game Logic"
+    global afterTime #s'indica que dintre aquesta funcó s'utilitzara la variable global afterTime
+    global FPS #s'indica que dintre aquesta funcó s'utilitzara la variable global FPS
+    nowTime = time.time() #Agafa els segons exactes que porta l'ordinador engegat
+    delta = (nowTime - afterTime) * FPS #Es resten els segons que porta l'ordinador engegat per els segosn que portava l'ordinador engegat l'última vegada que s'ha fet un bucle, per així saber cuanta estona porta el programa sense fer un bule. I segudament es multiplica per els FPS, i ens dona un valor anomenat delta.
+    if delta >= 1: #quan el valor delta es més gran o igual que 1, vol dir que ja toca fer un nou bucle i s'executa el codia que hi ha dinre aquest condicional
+        tdr_reader.rotate(1) #es crida la funció rotate del fitxer reader.py i se li passa el valor 1 perque els fotogrames s'incrementin un més
+        afterTime = nowTime #es fica el valor de la variable nowTime a la variable de la funcó afterTime
+
+
+def runningDown(cont): #funció que fa el mateix que la anteror, l'unica diferencia es que quan crida la funció rotate li passa un menys 1 perque es redueixi un futugrama. Basicament es el mateix pero marxa en rere
     global afterTime
-    if own["stat"] == "Animation_run":
-        FPS = 20
-        nowTime = time.time()
-        delta = (nowTime - afterTime) * FPS
-        if delta >= 1:
-            update()
-            afterTime = nowTime
+    global FPS
+    nowTime = time.time()
+    delta = (nowTime - afterTime) * FPS
+    if delta >= 1:
+        tdr_reader.rotate(-1)
+        afterTime = nowTime
+
             
 
-def init(cont):
-    global own
-    scene = bge.logic.getCurrentScene()
-    tdr_reader.openFile("prova_11_13.csv")
-    tdr_reader.openTable("correccio.csv")
-    tdr_reader.getScene(scene)
+def init(cont): #funció que es criada el principi del programma
+    global own #s'indica que dintre aquesta funcó s'utilitzara la funcó global own
+    scene = bge.logic.getCurrentScene() #s'agafa l'escena que està en aquest moment al Blender.
+    tdr_reader.openFile("prova_11_13.csv") #es crida la funció openFile del fitxer reader.py i se li passa el nom del fitxer que es desea obrir. Aquest fixter es el que conté els valors dels sensors de l'animació.
+    tdr_reader.openTable("calibration.csv") #aquest funcio es molt semblant a l'anterior, l'única diferencia és que en aquesta es passa el nom del fitxer de configuracío del gua
+    tdr_reader.getScene(scene) #aquesta funció bàsicament passa el valor scene a el fitxer reader.py
     #initAngles(scene)
-    cont1 = bge.logic.getCurrentController()
-    own = cont1.owner
-    own["stat"] = "Animation_run"
-    print("init")
-    tdr_reader.rotate(0)
+    cont.owner["stat"] = "Animation_run" #es modifica la propietat del "GameLogic" anomenada stat
+    print("init") #escriu a la consola de Blender init, per indicar que tot s'ha inicialitzat bé
+    tdr_reader.rotate(0) #es crida la funció rotate del fitxer reader.py i se li passa el valor 0 perque sense sumarse cap fotograma es situi be la mà a la posició zero del fitxer .csv
 
-def update():
-        tdr_reader.rotate(1)
+def reset(cont): #es una funcó que es cridada desde el "Game Logic" i que bàsicament reinicia el contador de fotogrames a zero
+   tdr_reader.reset() #crida la funcó reset de el fitxer reader.py
+
+def oneframePlus(cont): #es la funció que permet sumar un sol fotograma més clicant la "i". Aquesta funcó es cridada desde el "Game Logic"
+    sens = cont.sensors["Keyboard.001"] #es fica les propietats d'un sensor especific dintre d'una variable
+    if sens.positive: #basicament serveix perque només es cridi la funció una vegada quan s'ha clicat el boto corresponent
+        tdr_reader.rotate(1) ##es crida la funció rotate del fitxer reader.py i se li passa el valor 1 perque els fotogrames s'incrementin un més
+
+def oneframeLess(cont): #funció que fa el mateix que l'anteriorm l'únic que crida la funció rotate passant el valor menys 1, perque així es redueixi un fotograma
+    sens = cont.sensors["Keyboard.002"]
+    if sens.positive:
+        tdr_rotate.rotate(-1)
 
 #def initAngles(scene):
     #scene.objects.get("Mio_home").channels["Thumb1_R"].rotation_mode = bge.logic.ROT_MODE_XYZ
--- tdr_reader.py
+++ tdr_reader.py
@@ -1,25 +1,24 @@
-import csv
-import tdr_calcul
-import bge
+import csv #llibreries que permeten llegir fitxers .csv
+import tdr_calcul #s'imporen les funcións del fitxer calculations.py
+import bge #llibreria de blender
 
-frame = 0
-fileReader = []
-table = []
-scene = 0
+frame = 0 #variable global que conta per a quin fotograma va el programa
+fileReader = [] #variable array que contindrà tots els valors del sensor que cal aplicar
+table = [] #variable array que contindrà tots els valors de al configuració del gúa
+scene = 0 #variable global
 
-def openTable(table_name):
-    with open(table_name) as csvfile:
-        file1 = csv.reader(csvfile, delimiter=';')
-        global table
-        for row in file1:
-            table.append(row)
-        for x in range(len(table) -1):
-            print(x)
-            for i in range(25):
-                val = table[x + 1][i + 1]
-                val = val.replace(",",".")
-                val = float(val)
-                table[x + 1][i + 1] = val
+def openTable(table_name): #funció que s'ocupa de agafar la configurció del del fixer i passar-la a una array
+    with open(table_name) as csvfile: #fucnió que obr el fixer
+        file1 = csv.reader(csvfile, delimiter=';') #funció que passa el fitxer .csv a una array
+        global table #s'indica que dintre aquesta funcó s'utilitzara la funcó global table
+        for row in file1: #bucle que actuarà tantes vegades com lies hi ha el fitxer
+            table.append(row) #aplica cada linia a la variable global table
+        for x in range(len(table) -1): #bucle que actuarà tantes vegades com files menys una hi hagin
+            for i in range(25): #bucle que es fara 25 vegades
+                val = table[x + 1][i + 1] #es fica el valor de la rai en una determinada posicio a una variable
+                val = val.replace(",",".") #si el valor es decimal tindra una coma, i aquesta funcó o trandformara a punt
+                val = float(val) #aquesta funcó transorma un string a una variable float, per tant transforma un conjunt de caracters en un valor
+                table[x + 1][i + 1] = val #torna ficar el valor de la variable val dintre la mateixa posició 
 
 def openFile(file_name):
     with open(file_name) as csvfile:
@@ -41,28 +40,16 @@ def rotate(inc):
     frame = frame + inc
     if frame > len(fileReader)-6:
         frame = len(fileReader)-6
-        print("reset")
+        print("finich")
     if frame < 0:
         frame=0
-    print (fileReader[frame+2][0])
+    print(fileReader[frame + 2][0])
     tdr_calcul.calculations(table[2], table[3], table[4], fileReader[frame + 2], table[1], scene)
 
-def oneframePlus(cont):
-    global frame
-    sens = cont.sensors["Keyboard.001"]
-    if sens.positive:
-        rotate(1)
-
-def oneframeLess(cont):
-    global frame
-    sens = cont.sensors["Keyboard.002"]
-    if sens.positive:
-        rotate(-1)
-
-def reset(cont):
+def reset():
     global frame
     frame = 0
-    rotate()
+    rotate(0)
 
 def getScene(sc):
     global scene
